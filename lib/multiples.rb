ab70cc627cd124973b70bb94c3e7cfd0eef59db8
KentLeong <erickentleongchu@gmail.com>
2015-11-21 01:04:55 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,27 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  i = 0
+  array = []
+  new_array = []
+
+  # creates array 1 to number input
+  until i == limit - 1 do
+  i = i + 1
+  array << i
+  end
+
+# Takes out all numbers that arent divisible by 3 and 5
+  array.each do |index|
+    if index % 3 == 0
+      new_array << index
+    elsif index % 5 == 0
+      new_array << index
+    end
+  end
+
+return new_array
+end
+
+def sum_multiples(limit)
+  return collect_multiples(limit).inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,35 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(number)
+    @limit = number
+  end
+
+  def collect_multiples
+  i = 0
+  array = []
+  new_array = []
+
+  # creates array 1 to number input
+  until i == @limit - 1 do
+  i = i + 1
+  array << i
+  end
+
+# Takes out all numbers that arent divisible by 3 and 5
+  array.each do |index|
+    if index % 3 == 0
+      new_array << index
+    elsif index % 5 == 0
+      new_array << index
+    end
+  end
+
+  return new_array
+  end
+
+  def sum_multiples
+    return collect_multiples.inject(:+)
+  end
+end
+
+puts "#{Multiples.new(10).collect_multiples}"
